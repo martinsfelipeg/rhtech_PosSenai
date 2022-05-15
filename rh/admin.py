3e656dc934f2c10db14882f5ffdb570f9a735ef8
martinsfelipeg <martinsfelipeg@gmail.com>
2022-05-15 15:34:28 -0300

    Tabelas criadas.

--- rh/admin.py
+++ rh/admin.py
@@ -3,3 +3,7 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Departamento)
+admin.site.register(Nota_Fiscal)
+admin.site.register(Cargo)
+admin.site.register(Folha_Pag)
+admin.site.register(Colaborador)
--- rh/models.py
+++ rh/models.py
@@ -3,3 +3,34 @@ from django.db import models
 
 class Departamento(models.Model):
     nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+class Nota_Fiscal(models.Model):
+    Data = models.DateField()
+    Num_NF = models.IntegerField()
+    Valor = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return f'{self.Data} / ({self.Num_NF} - {self.Valor})'
+
+class Cargo(models.Model):
+    Cargo = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.Cargo}'
+
+class Folha_Pag(models.Model):
+    Data = models.DateField()
+    Hora_Extra = models.DecimalField(decimal_places=2, max_digits=10)
+    Pagamento = models.DecimalField(decimal_places=2, max_digits=10)
+
+    def __str__(self):
+        return f'{self.Data} / ({self.Hora_Extra} - {self.Pagamento})'
+
+class Colaborador(models.Model):
+    nome = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
